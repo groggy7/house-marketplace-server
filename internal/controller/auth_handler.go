01e150e9309f1cf482ac288edc5a267a5d4ef9e8
Serhat Kılbaş <serhatkilbas127@gmail.com>
2025-04-05 17:21:06 +0300

    Update auth token cookie settings to disable secure and HTTP-only flags for testing purposes
    - Change the secure and HTTP-only flags of the auth token cookie to false, allowing for non-secure connections and enabling client-side access during development.

--- internal/controller/auth_handler.go
+++ internal/controller/auth_handler.go
@@ -53,7 +53,7 @@ func (s *AuthHandler) Login(c *gin.Context) {
 		return
 	}
 
-	c.SetCookie("auth_token", token, 24*60*60*30, "/", "", true, true)
+	c.SetCookie("auth_token", token, 24*60*60*30, "/", "", false, false)
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Login successful",
 		"user": gin.H{
